68284902fefea208a44be9d2e73b2dda9d0bb9de
afyqeyzar <norafyqe@gmail.com>
2022-10-17 11:10:33 +0100

    tophat func

--- waves-on-string/tophat.py
+++ waves-on-string/tophat.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def tophat(start, end, end_of_graph):
-  ini_plot_x = np.arange(0, end_of_graph, 0.1)
+def tophat(start, end, end_of_graph, density):
+  ini_plot_x = np.arange(0, end_of_graph, density)
   y = [0] * len(ini_plot_x)
   for idx, x in enumerate(ini_plot_x):
     if x >= start and x <= end:
@@ -10,7 +10,7 @@ def tophat(start, end, end_of_graph):
     
   return ini_plot_x, y
 
-x, y = tophat(2, 4, 10)
+x, y = tophat(2, 4, 10, 0.1)
 
 
 print(x)
